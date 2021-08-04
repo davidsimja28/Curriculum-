ddee165e479ea1588a4e5218ae8c13ef1f0fa370
David <20183l301023@utcv.edu.mx>
2021-08-03 20:16:51 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Documentación del proyecto
+- Creación del proyecto 
+- Ruteo de los componentes
+- Creación de la carpeta pages para el uso correcto de cada componente de la app
---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,14 @@
+import { InicioComponent } from './Pages/inicio/inicio.component';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+
+  {path: '', component: InicioComponent},
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
